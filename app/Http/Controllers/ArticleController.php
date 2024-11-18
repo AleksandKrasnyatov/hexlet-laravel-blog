ba44ad63b7af60bce767cd2df90137fcff98fc87
alex <ssokoloss1@gmail.com>
2024-11-18 09:46:43 +0300

    add article show page

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
 
 class ArticleController extends Controller
 {
-    public function index()
+    public function index(): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
     {
         $articles = Article::paginate();
         return view('article.index', compact('articles'));
+    }
+
+
+    public function show($id): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
+    {
+        $article = Article::findOrFail($id);
+        return view('article.show', compact('article'));
     }
 }
